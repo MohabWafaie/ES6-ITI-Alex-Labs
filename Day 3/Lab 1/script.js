b4778a03d6d24af3a6fcacf85410371a03d66c78
MohabWafaie <mohab.wafaie@gmail.com>
2025-06-12 23:39:23 +0300

    Tasks Solution

--- Day 3/Lab 1/script.js
+++ Day 3/Lab 1/script.js
@@ -1,1 +1,13 @@
-let x = "testing the branch"
+let food = ['Burger', 'Pizza', 'Donuts', 'Pizza', 'Koshary', 'Donuts', 'Seafood','Burger']
+let set = new Set(food)
+set.add('Pasta')
+console.log(set);
+set.delete('Burger')
+console.log(set)
+let checkSet = (set) => {
+    if (set.size > 2) {
+        set.clear();
+    }
+}
+checkSet(set);
+console.log(set);
